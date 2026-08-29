623cc7e063abc19ae5702523a27f1afab9577cb2
diego221992 <diegoalmeidaunlock@gmail.com>
2026-08-28 21:09:25 -0400

    Delete public directory

--- public/config.js
+++
@@ -1,5 +0,0 @@
-window.APP_CONFIG = {
-  //API_URL: "http://192.168.0.100:3002",
-  API_URL: "https://api.applock.online", // O el puerto donde corra tu backend (Node.js, PHP, etc.)
-  ENVIRONMENT: "development"
-};
