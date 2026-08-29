4598700f5b67e98bd50c5e1c60a006ae01b99931
diego221992 <diegoalmeidaunlock@gmail.com>
2026-08-28 21:11:38 -0400

    Add files via upload

---
+++ public/config.js
@@ -0,0 +1,5 @@
+window.APP_CONFIG = {
+  //API_URL: "http://192.168.0.100:3002",
+  API_URL: "https://server-api-mexico-vqre.onrender.com", // O el puerto donde corra tu backend (Node.js, PHP, etc.)
+  ENVIRONMENT: "development"
+};
